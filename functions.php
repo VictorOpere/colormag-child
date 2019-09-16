a182242484c153ae96c52513a1f06ab488689df1
Victor Opere <victoropere@gmail.com>
2019-09-16 19:47:27 +0300

    just added the other files

--- functions.php
+++ functions.php
@@ -1,15 +1,28 @@
 <?php
 
+/**
+ * 
+ * Enqueue the relevant scripts and stylesheets for the child theme
+ */
+
 function karavic_theme_enqueue_styles() {
  
     $parent_style = 'parent-style'; // This is 'colormag-style' for the Color Mag Theme.
- 
+   
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
-}
-
+   }
+   
 add_action( 'wp_enqueue_scripts', 'karavic_theme_enqueue_styles' );
+
+/**
+ *Additional removal of WordPress default options
+
+ */
+
+ 
+
